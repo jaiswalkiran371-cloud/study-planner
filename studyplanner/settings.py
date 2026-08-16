000cb0ffddce903f0ea5876fe6e951d378aa5e63
jaiswalkiran371-cloud <jaiswalkiran371@gmail.com>
2026-08-16 12:02:57 +0530

    Production-ready settings for deployment

--- studyplanner/settings.py
+++ studyplanner/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for studyplanner project.
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-^es_y$4%!)on(0*y6o5-w-)+hi4b7ls2-p%tl8z1q%z19d77v@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
